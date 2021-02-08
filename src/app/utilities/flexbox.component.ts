f07cb83fafa478f5ae7e3cf1ad7fd31305278be8
Yiğit FINDIKLI <yigitfindikli@gmail.com>
2021-02-08 14:01:13 +0300

    flexdemo updated

--- src/app/utilities/flexbox.component.ts
+++ src/app/utilities/flexbox.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 import {BreadcrumbService} from '../app.breadcrumb.service';
 
 @Component({
-    templateUrl: './flexbox.component.html'
+    templateUrl: './flexbox.component.html',
+    styleUrls: ['./flexbox.scss']
 })
 export class FlexboxComponent {
     constructor(private breadcrumbService: BreadcrumbService) {
